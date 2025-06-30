d11cb2002440afc85a31ddcf6b7051df4175a9b8
Sudeep <sudeepsinha337@gmail.com>
2025-06-30 18:32:34 +0530

    PostForm commit

--- src/components/index.js
+++ src/components/index.js
@@ -8,11 +8,14 @@ import Logo from "./Logo";
 
 import LogoutBtn from "./Header/LogoutBtn";
 
+import RTE from "./RTE";
+
 
 
 export { Header, 
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE
 };
